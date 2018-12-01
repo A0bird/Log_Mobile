ef82e57dc2d801ae9998b410084201ea3734e365
dongge <775600040@qq.com>
2018-12-01 17:05:08 +0800

    this is first commit.

---
+++ src/main/java/com/mobile/Test.java
@@ -0,0 +1,14 @@
+package com.mobile;
+
+/**
+ * @ClassName Test
+ * @Author lyd
+ * @Date $ $
+ * @Vesion 1.0
+ * @Description //TODO $
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
